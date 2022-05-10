e5b982c6dd20b6c38ccd1060726c37315c96a1c0
Anhisa <anhisa@hotmail.com>
2022-05-09 21:18:38 -0500

    axios working

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { AccountItem } from './AccountItem';
 import axios from 'axios';
 
-const api = '/official-accounts'
+const api =
+  'https://fundacionandresbello.local/wp-json/fab/v1/official-accounts';
 
 const AccountList = () => {
-  const [account, setAccount] = useState([]);
+  const [accounts, setAccounts] = useState([]);
 
   useEffect(async () => {
     const response = await axios(api);
-    setAccount(response.data);
-  }, [])
+    setAccounts(response.data);
+  }, []);
 
   return (
     <section>
       <div>
+        {accounts.map((account) => (
+          <>
+            <h2>{account.official_account}</h2>
+            <h3>{account.official_account_name_spa}</h3>
+            <h3>{account.country_name_spa}</h3>
+          </>
+        ))}
         <AccountItem />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export {AccountList};
+export { AccountList };
