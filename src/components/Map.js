7faf12ca93c59e1c353db38c425f028945338089
Anhisa <anhisa@hotmail.com>
2022-05-23 11:52:03 -0500

    title changed, map with zoom, markers removed

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ import {
   Geographies,
   Geography,
   Marker,
-  // ZoomableGroup
+  ZoomableGroup,
 } from 'react-simple-maps';
 
 const geoUrl =
@@ -12,47 +12,91 @@ const geoUrl =
 
 const api = 'https://fundacionandresbello.org/wp-json/fab/v1/official-accounts';
 
+  export const Map = ({ setAccounts, items }) => {
+    const [position, setPosition] = useState({ coordinates: [-75, -10], zoom: 1 });
 
-export const Map = ({ setAccounts, items }) => {
-  // const response = useGetData(api);
-  // const items = response.data;
+    function handleZoomIn() {
+      if (position.zoom >= 4) return;
+      setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.1 }));
+    }
 
-  const handleOnClick = (props) => {
-    const itemValue = props.target.attributes.value;
-    const filteredAccounts = items.filter(
-      (item) => item.country_id === itemValue.value
+    function handleZoomOut() {
+      if (position.zoom <= 1) return;
+      setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.1 }));
+    }
+
+    function handleMoveEnd(position) {
+      setPosition(position);
+    }
+    // const response = useGetData(api);
+    // const items = response.data;
+
+    const handleOnClick = (props) => {
+      const itemValue = props.target.attributes.value;
+      const filteredAccounts = items.filter(
+        (item) => item.country_id === itemValue.value
+      );
+      console.log(props.clientX, props.clientY);
+      setAccounts(filteredAccounts);
+    };
+
+    return (
+        <><ComposableMap
+        projection="geoAzimuthalEqualArea"
+        projectionConfig={{
+          rotate: [73, 11, 0],
+          scale: 587,
+        }}
+      >
+        <ZoomableGroup
+          zoom={position.zoom}
+          center={position.coordinates}
+          onMoveEnd={handleMoveEnd}
+        >
+          <Geographies geography={geoUrl} style={{ cursor: 'pointer' }}>
+            {({ geographies }) => geographies
+              .filter(
+                (d) => d.properties.REGION_WB === 'Latin America & Caribbean'
+              )
+              .map((geo) => (
+                <Geography
+                  className="geo"
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#BBB"
+                  value={geo.properties.COUNTRY_ID}
+                  stroke="#D6D6DA"
+                  onClick={handleOnClick} />
+              ))}
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap><div className="controls">
+          <button onClick={handleZoomIn}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="3"
+            >
+              <line x1="12" y1="5" x2="12" y2="19" />
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </button>
+          <button onClick={handleZoomOut}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="3"
+            >
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </button>
+        </div></>
     );
-    console.log(props.clientX, props.clientY);
-    setAccounts(filteredAccounts);
   };
 
-  return (
-    <ComposableMap
-      projection="geoAzimuthalEqualArea"
-      projectionConfig={{
-        rotate: [73, 11, 0],
-        scale: 587,
-      }}
-    >
-      <Geographies geography={geoUrl} style={{ cursor: 'pointer' }}>
-        {({ geographies }) =>
-          geographies
-            .filter(
-              (d) => d.properties.REGION_WB === 'Latin America & Caribbean'
-            )
-            .map((geo) => (
-              <Geography
-                className="geo"
-                key={geo.rsmKey}
-                geography={geo}
-                fill="#BBB"
-                value={geo.properties.COUNTRY_ID}
-                stroke="#D6D6DA"
-                onClick={handleOnClick}
-              />
-            ))
-        }
-      </Geographies>
-    </ComposableMap>
-  );
-};
