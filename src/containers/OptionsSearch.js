de744f733c1b3842ab5bb6250776fa2726d157d5
David Charif <davidcharif@hotmail.com>
2022-06-14 14:48:43 -0500

    Scroll to monthy tweets after opening tables

--- src/containers/OptionsSearch.js
+++ src/containers/OptionsSearch.js
@@ -2,17 +2,17 @@ import { Button } from '@mui/material';
 import React from 'react';
 import handleClick from '../helpers/HandleClick';
 
-const OptionsSearch = ({setDataComparing, context}) => {
+const OptionsSearch = ({ setDataComparing, context }) => {
   const [open, setOpen] = React.useState(false);
 
-const {accounts, periodComparison} = context;
-const {accountIdA, accountIdB} = accounts;
-const {periodA, periodB} = periodComparison;
-if(accountIdA.id === accountIdB.id ){
-  return []
-}
-  function handleClear(){
-    setDataComparing(prev => {
+  const { accounts, periodComparison } = context;
+  const { accountIdA, accountIdB } = accounts;
+  const { periodA, periodB } = periodComparison;
+  if (accountIdA.id === accountIdB.id) {
+    return [];
+  }
+  function handleClear() {
+    setDataComparing((prev) => {
       return {
         ...prev,
         accounts: {
@@ -27,37 +27,59 @@ if(accountIdA.id === accountIdB.id ){
         },
         periodComparison: {
           periodA: {
-            id:'',
+            id: '',
             name: '',
           },
           periodB: {
-            id:'',
+            id: '',
             name: '',
           },
-        }
-      }
-    })
+        },
+      };
+    });
   }
-  function openTables(){
+  function openTables() {
     const options = [
-      'monthy-tweets', 'most-retweet', 'most-ht', 'most-mentioned', 'most-replied',
-    ]    
-    
-    options.forEach(option => {
-      handleClick(option, open);
-    })
-    setOpen(prev => !prev);
-    if(open){
-      let element =  document.getElementById('monthy-tweets')
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
+      'monthy-tweets',
+      'most-retweet',
+      'most-ht',
+      'most-mentioned',
+      'most-replied',
+    ];
 
+    options.forEach((option) => {
+      let element = document.getElementById(option);
+      if (open) {
+        if (element.classList.contains('open')) {
+          element.classList.remove('open');
+          element.classList.add('closed');
+        }
+        return;
+      } else {
+        if (element.classList.contains('closed')) {
+          element.classList.remove('closed');
+          element.classList.add('open');
+        }
+        return;
+      }
+    });
+    let element = document.getElementById('monthy-tweets');
+    element.scrollIntoView({ behavior: 'smooth' });
+    setOpen((prev) => !prev);
+    // if (open) {
+    //   let element = document.getElementById('monthy-tweets');
+    //   element.scrollIntoView({ behavior: 'smooth' });
+    // }
+  }
 
   return (
     <>
-      <Button variant="contained" onClick={handleClear}>Limpiar busqueda</Button>
-      <Button variant="contained" onClick={openTables}>{!open ? 'Abrir' : 'Cerrar'} tablas</Button>
+      <Button variant="contained" onClick={handleClear}>
+        Limpiar busqueda
+      </Button>
+      <Button variant="contained" onClick={openTables}>
+        {!open ? 'Abrir' : 'Cerrar'} tablas
+      </Button>
     </>
   );
 };
